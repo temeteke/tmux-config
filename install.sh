3f8d24436a1432f424f2bc834f53d6ec45a81ee3
temeteke <temeteke@gmail.com>
2013-11-25 17:36:54 +0900

    Add settings for xclip.

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 cd `dirname $0`
 
+version=`tmux -V | awk '{print $2}'`
+
 if [ -f ~/.tmux.conf ]; then
 	rm ~/.tmux.conf
 fi
@@ -16,6 +18,17 @@ fi
 
 if [ -f tmux.conf ]; then
 	cat tmux.conf >> ~/.tmux.conf
+fi
+
+if [ `echo "$version >= 1.8" | bc` -eq 1 ] && which xclip > /dev/null 2>&1; then
+	cat >> ~/.tmux.conf <<-_EOT_
+
+		bind-key -t vi-copy v begin-selection
+		bind-key -t vi-copy y copy-pipe "xclip"
+
+		unbind -t vi-copy Enter
+		bind-key -t vi-copy Enter copy-pipe "xclip"
+	_EOT_
 fi
 
 tmux source-file ~/.tmux.conf
